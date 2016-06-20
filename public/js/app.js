4c2a40cc275991924e8bd5ed3473f72a4fd1274e
Tessa Serdar <tessajeanserdar@gmail.com>
2016-06-19 19:53:47 -0700

    Adds modes object to Boars object in order to allow different levels of game play

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,21 @@
 var myApp = angular.module('myApp', ['BoardService']);
 myApp.controller('CalculatorController', function ($scope, Board) {
     $scope.initGame = function(mode){
-      mode = mode || 'normal';
-      Board.initBoard(Board.gameMode[mode].size,Board.gameMode[mode].mines);
+      $scope.mode = mode;
       $scope.status = "live";
       $scope.cheatEnabled = false;
-      Board.totalUncovered = 0;
+      Board.clearBoard();
+      Board.initBoard(Board.gameMode[mode].size,Board.gameMode[mode].mines,mode);
       $scope.rows = Board.rows;
     }
     $scope.visit = function (cell) {
       if(cell.isMine){
         $scope.status = "lost";
       }
-      Board.getConnectedNonMineCells(cell)
+      Board.getConnectedNonMineCells(cell,$scope.mode)
     }
     $scope.validateBoard = function(){
-      if(Board.totalUncovered === Board.squareCount - Board.mines && $scope.status !== "lost") {
+      if(Board.totalUncovered === Board.gameMode[$scope.mode].totalSquares - Board.gameMode[$scope.mode].mines && $scope.status !== "lost") {
         $scope.status = "won"
       } else  {
         $scope.status = "lost";
--- public/js/factory.js
+++ public/js/factory.js
@@ -1,86 +1,94 @@
 var BoardService = angular.module('BoardService', [])
 .service('Board', function () {
-    this.gameSize  = 8;
-    this.squareCount = this.gameSize * this.gameSize
+    this.mines = null;
     this.rows = [];
-    this.mines = 10;
     this.totalUncovered = 0;
-    this.gameMode = {
+    this.gameMode =  {
       normal: {size : 8, mines : 10}, 
       easy: {size : 4, mines: 4},
       medium: {size : 10, mines: 20},
-      hard: {size : 12, mines: 40}
+      hard: {size : 12, mines: 40} 
     }
-    this.addMines = function (mines){
+    this.gameMode.normal.totalSquares = this.gameMode.normal.size * this.gameMode.normal.size;
+    this.gameMode.easy.totalSquares = this.gameMode.easy.size * this.gameMode.easy.size; 
+    this.gameMode.medium.totalSquares = this.gameMode.medium.size * this.gameMode.medium.size; 
+    this.gameMode.hard.totalSquares = this.gameMode.hard.size * this.gameMode.hard.size; 
+    this.addMines = function (mines,mode){
       var minesCreated = 0;
       var rowLocation;
       var colLocation;
+      console.log(mode)
+      console.log(this.gameMode[mode].size)
       while (minesCreated < mines){
-        rowLocation = Math.floor(Math.random() * this.gameSize);
-        colLocation = Math.floor(Math.random() * this.gameSize);
+        rowLocation = Math.floor(Math.random() * this.gameMode[mode].size);
+        colLocation = Math.floor(Math.random() * this.gameMode[mode].size);
         if(!this.rows[rowLocation][colLocation].isMine){
           this.rows[rowLocation][colLocation].isMine = true;
-          this.addToAdjacentCellsMinesCount(rowLocation,colLocation);
+          this.addToAdjacentCellsMinesCount(rowLocation,colLocation,mode);
           minesCreated++;
         }
       }
     }
-    this.addToAdjacentCellsMinesCount = function (r,c){
+    this.clearBoard = function (){
+       this.rows = [];
+       this.totalUncovered = 0;
+    }
+    this.addToAdjacentCellsMinesCount = function (r,c,mode){
       if ( r > 0 && c > 0 ) {
         this.rows[r-1][c-1].minesAdjacentCount++;
       } 
       if ( r > 0 ) {
         this.rows[r-1][c].minesAdjacentCount++;
       }
-      if( r > 0 && c < this.gameSize - 1) {
+      if( r > 0 && c < this.gameMode[mode].size - 1) {
         this.rows[r-1][c+1].minesAdjacentCount++;
       }
       if ( c > 0 ) {
         this.rows[r][c-1].minesAdjacentCount++;
       }
-      if ( c > 0 && r < this.gameSize - 1) {
+      if ( c > 0 && r < this.gameMode[mode].size - 1) {
         this.rows[r+1][c-1].minesAdjacentCount++;
       }
-      if ( c < this.gameSize -1 && r < this.gameSize -1 ) {
+      if ( c < this.gameMode[mode].size -1 && r < this.gameMode[mode].size -1 ) {
         this.rows[r+1][c+1] .minesAdjacentCount++;    
       }
-      if ( c < this.gameSize - 1 ) {
+      if ( c < this.gameMode[mode].size - 1 ) {
         this.rows[r][c+1].minesAdjacentCount++;
       }
-      if ( r < this.gameSize -1 ) {
+      if ( r < this.gameMode[mode].size -1 ) {
         this.rows[r+1][c].minesAdjacentCount++;
       }
     }
-    this.getConnectedNonMineCells = function(cell){
+    this.getConnectedNonMineCells = function(cell,mode){
       cell.isRevealed = true;
       this.totalUncovered++;
       for (var i = -1 ; i < 2 ; i++) {
         for (var j = -1; j < 2 ; j++) {
           var row = cell.rowNum + i;
           var column = cell.colNum + j;
-          if ((row >= 0) && (column >= 0) && (row < this.gameSize) && (column < this.gameSize)) {
+          if ((row >= 0) && (column >= 0) && (row < this.gameMode[mode].size) && (column < this.gameMode[mode].size)) {
             var current = this.rows[row][column];
             if (!current.isMine && !current.isRevealed) {
-              this.totalUncovered++
+              this.totalUncovered++;
               current.isRevealed = true;
             }
           }
         }
       }
     }
-    this.initBoard = function(size,mines){
-        for (var i = 0; i < size; i++) {
-          this.rows[i] = new Array(size);
-          for (var j = 0; j < size; j++) {
-            this.rows[i][j] = {
-              isMine: false,
-              isRevealed : false,
-              minesAdjacentCount : null,
-              rowNum : i,
-              colNum : j
-            };
-          }
+    this.initBoard = function(size,mines,mode){
+      for (var i = 0; i < size; i++) {
+        this.rows[i] = new Array(size);
+        for (var j = 0; j < size; j++) {
+          this.rows[i][j] = {
+            isMine: false,
+            isRevealed : false,
+            minesAdjacentCount : null,
+            rowNum : i,
+            colNum : j
+          };
         }
-        this.addMines(mines);
+      }
+      this.addMines(mines,mode);
     } 
 });
