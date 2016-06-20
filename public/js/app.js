8a27de70b7cc1bd35de20a733aef015ffcd5dd7d
Tessa Serdar <tessajeanserdar@gmail.com>
2016-06-19 20:07:03 -0700

    Fixes spacing errors

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,29 @@
 var myApp = angular.module('myApp', ['BoardService']);
 myApp.controller('CalculatorController', function ($scope, Board) {
-    $scope.initGame = function(mode){
-      $scope.mode = mode;
-      $scope.status = "live";
-      $scope.cheatEnabled = false;
-      Board.clearBoard();
-      Board.initBoard(Board.gameMode[mode].size,Board.gameMode[mode].mines,mode);
-      $scope.rows = Board.rows;
+  $scope.initGame = function(mode){
+    $scope.mode = mode;
+    $scope.status = "live";
+    $scope.cheatEnabled = false;
+    Board.clearBoard();
+    Board.initBoard(Board.gameMode[mode].size,Board.gameMode[mode].mines,mode);
+    $scope.rows = Board.rows;
+  }
+  $scope.visit = function (cell) {
+    if(cell.isMine){
+      $scope.status = "lost";
     }
-    $scope.visit = function (cell) {
-      if(cell.isMine){
-        $scope.status = "lost";
-      }
-      Board.getConnectedNonMineCells(cell,$scope.mode)
+    Board.getConnectedNonMineCells(cell,$scope.mode)
+  }
+  $scope.validateBoard = function(){
+    if(Board.totalUncovered === Board.gameMode[$scope.mode].totalSquares - Board.gameMode[$scope.mode].mines && $scope.status !== "lost") {
+      $scope.status = "won"
+    } else  {
+      $scope.status = "lost";
     }
-    $scope.validateBoard = function(){
-      if(Board.totalUncovered === Board.gameMode[$scope.mode].totalSquares - Board.gameMode[$scope.mode].mines && $scope.status !== "lost") {
-        $scope.status = "won"
-      } else  {
-        $scope.status = "lost";
-      }
-    }
-    $scope.setCheatMode = function(){
-      $scope.cheatEnabled = !$scope.cheatEnabled;
-    }
-    $scope.initGame('normal');
+  }
+  $scope.setCheatMode = function(){
+     $scope.cheatEnabled = !$scope.cheatEnabled;
+  }
+  $scope.initGame('normal');
      
 });
